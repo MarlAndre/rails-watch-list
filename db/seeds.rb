fc5e16e8f6b695f36532d11a1a418749637a9e5a
Marlene Andre <andre.marlene1990@gmail.com>
2022-04-21 20:38:49 -0400

    homepage done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Movie.destroy_all
+List.destroy_all
+
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+List.create(name: "Classical", image_url: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/04/03/12/Casablanca.jpg?quality=75&width=990&auto=webp&crop=982:726,smart")
+List.create(name: "Superhero", image_url: "https://www.denofgeek.com/wp-content/uploads/2017/11/thor_ragnarok_comedy_marvel-1.jpg?resize=768%2C432")
+List.create(name: "Queer romance", image_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-lgbt-films-portrait-of-a-lady-on-fire-everett-1589819041.jpg")
