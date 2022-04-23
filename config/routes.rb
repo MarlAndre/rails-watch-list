f98a6ae3bf252c7f394b9b01de85879978c8a415
Marlene Andre <andre.marlene1990@gmail.com>
2022-04-23 17:04:33 -0400

    add bookmarks view and some css

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   resources :lists, only: [ :index, :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
-  resources :bookmarks, only: [ :destroy ]
   resources :movies
-
 end
